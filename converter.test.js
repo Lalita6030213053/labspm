b5d52519f67b89233d4dc3681f2d31c78d810b6f
Lalita6030213053 <ja.ea5923@gmail.com>
2019-11-02 13:12:45 +0700

    run test

--- converter.test.js
+++ converter.test.js
@@ -1,17 +1,21 @@
 const converter = require('./converter.js');
 
-test('return $0.00 when input is 0', () => {
+test('$0.00 = ฿0', () => {
     expect(converter(0)).toEqual('$0.00');
 });
 
-test('return $ 0.03 when input is 1', () => {
+test('$0.03 = ฿1', () => {
     expect(converter(1)).toEqual('$0.03');
 });
 
-test('return $ -0.03 when input is -1', () => {
-    expect(converter(-1)).toEqual('-$0.03');
+test('$0.09 = ฿3', () => {
+    expect(converter(3)).toEqual('$0.09');
 });
 
-test('return ERROR when input is String', () => {
-    expect(converter('String')).toEqual('ERROR');
+test('$0.15 = ฿5', () => {
+    expect(converter(5)).toEqual('$0.15');
+});
+
+test('$0.21 = ฿7', () => {
+    expect(converter(7)).toEqual('$0.21');
 });
